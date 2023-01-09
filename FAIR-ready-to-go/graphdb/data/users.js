5350f71fa3c19d124429be9fbc1ff348b59921ea
Mark Wilkinson <markw@illuminae.com>
2023-01-09 11:17:24 +0100

    default inde x

---
+++ FAIR-ready-to-go/graphdb/data/users.js
@@ -0,0 +1,17 @@
+{
+  "users" : {
+    "admin" : {
+      "username" : "admin",
+      "password" : "{bcrypt}$2a$10$nzcTD5bbGQeacJgzZsX4P.C6gda08Y17m21Ujh3q46FHsQ.UNnrN2",
+      "grantedAuthorities" : [ "ROLE_ADMIN" ],
+      "appSettings" : {
+        "DEFAULT_INFERENCE" : true,
+        "DEFAULT_VIS_GRAPH_SCHEMA" : true,
+        "DEFAULT_SAMEAS" : true,
+        "IGNORE_SHARED_QUERIES" : false,
+        "EXECUTE_COUNT" : true
+      },
+      "dateCreated" : 1670834184349
+    }
+  }
+}
